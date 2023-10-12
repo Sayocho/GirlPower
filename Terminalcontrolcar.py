cb9815dea0a2a6df48f049da7b902ecb2e31ea99
“6redac6” <sofie1997miller@gmial.com>
2023-10-12 11:51:25 +0200

    yes

--- Terminalcontrolcar.py
+++ Terminalcontrolcar.py
@@ -1,10 +1,10 @@
 from gpiozero import Motor
 import curses
 
-flmotor = Motor(forward=16, backward=17) #rasppin: 36(GPIO16) & 11(GPIO17)
-frmotor = Motor(forward=18, backward=13) #rasppin: 12(GPIO18) & 33(GPIO13)
-blmotor = Motor(forward=9, backward=11) #rasppin: 21(GPIO9) & 23(GPIO11)
-brmotor = Motor(forward=10, backward=12) #rasppin: 19(GPIO10) & 32(GPIO12)
+flmotor = Motor(forward=16, backward=17) #RPIN: 36(GPIO16) & 11(GPIO17)
+frmotor = Motor(forward=18, backward=13) #RPIN: 12(GPIO18) & 33(GPIO13)
+blmotor = Motor(forward=9, backward=11)  #RPIN: 21(GPIO9)  & 23(GPIO11)
+brmotor = Motor(forward=10, backward=12) #RPIN: 19(GPIO10) & 32(GPIO12)
 
 def left():
 #    print('Left ...')
