d05861a003ffd417eacf921c611be8207a9e1355
“6redac6” <sofie1997miller@gmial.com>
2023-10-13 01:56:26 +0200

    yes

--- Terminalcontrolcar.py
+++ Terminalcontrolcar.py
@@ -1,45 +1,45 @@
-from gpiozero import Motor
-import curses
+from gpiozero import Motor #Controling DC motors using GPIO
+import curses #Controling the car by using terminal
 
-flmotor = Motor(forward=13, backward=17)   #RPIN: 33(GPIO13) & 7 (GPIO4)  Forward = PWM Backward = DIR:
-frmotor = Motor(forward=19, backward=4)  #RPIN: 35(GPIO19) & 11(GPIO17) Forward = PWM Backward = DIR:
+flmotor = Motor(forward=13, backward=17)  #RPIN: 33(GPIO13) & 7 (GPIO4)  Forward = PWM Backward = DIR:
+frmotor = Motor(forward=19, backward=4)   #RPIN: 35(GPIO19) & 11(GPIO17) Forward = PWM Backward = DIR:
 blmotor = Motor(forward=12, backward=23)  #RPIN: 32(GPIO12) & 15(GPIO22) Forward = PWM Backward = DIR:
 brmotor = Motor(forward=18, backward=22)  #RPIN: 12(GPIO18) & 16(GPIO23) Forward = PWM Backward = DIR:
 
 def left():
-#    print('Left ...')
-    flmotor.backward()
-    frmotor.forward()
-    blmotor.backward()
-    brmotor.forward()
+#    Left 
+    flmotor.backward(-1)
+    frmotor.forward(1)
+    blmotor.backward(-1)
+    brmotor.forward(1)
 
 def right():
-#    print('Right ...')
-    flmotor.forward()
-    frmotor.backward()
-    blmotor.forward()
-    brmotor.backward()
+#    Right 
+    flmotor.forward(1)
+    frmotor.backward(-1)
+    blmotor.forward(1)
+    brmotor.backward(-1)
 
 def forward():
-#    print('Forwarding ...')
-    flmotor.forward()
-    frmotor.forward()
-    blmotor.forward()
-    brmotor.forward()
+#    Forwarding 
+    flmotor.forward(1)
+    frmotor.forward(1)
+    blmotor.forward(1)
+    brmotor.forward(1)
 
 def reverse():
-#    print('Reversing ...')
-    flmotor.backward()
-    frmotor.backward()
-    blmotor.backward()
-    brmotor.backward()
+#    Reversing 
+    flmotor.backward(-1)
+    frmotor.backward(-1)
+    blmotor.backward(-1)
+    brmotor.backward(-1)
 
 def stop():
-#    print('Stopping ...')
-    flmotor.stop()
-    frmotor.stop()
-    blmotor.stop()
-    brmotor.stop()
+#    Stopping 
+    flmotor.stop(0)
+    frmotor.stop(0)
+    blmotor.stop(0)
+    brmotor.stop(0)
 
 actions = {
     curses.KEY_UP:    forward,
@@ -57,7 +57,7 @@ def main(window):
         else:
             key = next_key
             next_key = None
-        if key != -1:
+        if key != -1:       #-1 full speed one dirction (1 = full speed the other direction. 0= stop)
             # KEY PRESSED
             curses.halfdelay(3) #Orginal 3
             action = actions.get(key)
