6307a7c012887c43de930d032cc8c4a2090e6a98
“6redac6” <sofie1997miller@gmial.com>
2023-10-13 08:27:00 +0200

    yes

--- Terminalcontrolcar.py
+++ Terminalcontrolcar.py
@@ -1,18 +1,18 @@
 from gpiozero import Motor #Controling DC motors using GPIO
 import curses #Controling the car by using terminal
-"""
+
 flmotor = Motor(forward=13, backward=17)  #RPIN: 33(GPIO13) & 7 (GPIO4)  Forward = PWM Backward = DIR:
 frmotor = Motor(forward=19, backward=4)   #RPIN: 35(GPIO19) & 11(GPIO17) Forward = PWM Backward = DIR:
 blmotor = Motor(forward=12, backward=23)  #RPIN: 32(GPIO12) & 15(GPIO22) Forward = PWM Backward = DIR:
 brmotor = Motor(forward=18, backward=22)  #RPIN: 12(GPIO18) & 16(GPIO23) Forward = PWM Backward = DIR:
-"""
 
+"""
 flmotor = Motor(forward=17, backward=13)
 frmotor = Motor(forward=4,  backward=19)
 blmotor = Motor(forward=23, backward=12)
 brmotor = Motor(forward=22, backward=18)
 
-
+"""
 def left():
 #    Left 
     flmotor.backward()
